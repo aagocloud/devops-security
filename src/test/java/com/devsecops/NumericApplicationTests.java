42a6ed2eb774369ef05ca3cf57da9a7935449827
aagocloud <pubudu.senadeera@gmail.com>
2023-10-16 11:40:38 +0530

    corrected unit test issue

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -49,7 +49,7 @@ public class NumericApplicationTests {
     @Test
     public void welcomeMessage() throws Exception {
          this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string("Greater than 50"));
+                .andExpect(content().string("Kubernetes DevSecOps"));
     }
     
 
