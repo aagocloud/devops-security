8fb74fc9866a72e9820d66e0dd4c90f7883ef4e8
aagocloud <pubudu.senadeera@gmail.com>
2023-10-17 12:05:20 +0530

    fix sh file

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -7,7 +7,7 @@ docker run -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.45.1 -q image --exit-code
 docker run -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.45.1 -q image --exit-code 1 --severity CRITICAL --light $dockerBaseImage
 
     # Trivy scan result processing
-    exit_code =$?
+    exit_code=$?
     echo "Exit code : $exit_code"
 
     if [[ "${exit_code}" == 1 ]]; then
